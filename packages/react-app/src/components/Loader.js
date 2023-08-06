c2bd2575331cd89ed8eb7c912f1c9b58dbfa0be4
Mehmed Muric <mehmedmuric22@gmail.com>
2023-08-06 20:31:48 +0200

    loader...

---
+++ packages/react-app/src/components/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ethereumLogo } from '../assets';
+import styles from '../styles';
+
+const Loader = ({title}) => {
+  return (
+    <div className={styles.loader}>
+      <img src={ethereumLogo} alt='eth_logo' className={styles.loaderImg}/>
+      <p className={styles.loaderText}>{title}</p>
+    </div>
+  )
+}
+
+export default Loader
